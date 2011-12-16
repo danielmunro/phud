19044ef4fa4b273bf7ef1f3dc030339ae5c20f82
Dan Munro <dan@danmunro.com>
2011-12-16 02:46:58 +0000

    removing stuff from websocket work

--- docroot/index.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-<script type="text/javascript" src="client.js"></script>
-<canvas id="frame" style="position: absolute; top: 0px; left: 0px; width: 900px; height: 500px; border: 1px solid black;"></canvas>
-<div id="out" style="position: absolute; top: 500px; height: 200px; width: 900px; border: 1px solid black; overflow-y: scroll;"></div>
-<br />
-<input type="text" style="position:absolute; top: 700px; width: 900px;" id="input" />
