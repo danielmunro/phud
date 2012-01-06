21ab08f465424d0861c0c502252505619e5542f9
Dan Munro <dan@danmunro.com>
2012-01-06 14:03:30 +0000

    Fixing up attset to work with the new attributes class

--- lib/Commands/AttSet.php
+++ lib/Commands/AttSet.php
@@ -42,103 +42,32 @@
 		public function perform(lUser $user, $args = array())
 		{
 			$object = $user->getRoom()->getActorByInput($args[1]);
-			if(!$object)
+			if(!$object) {
 				$object = $user->getRoom()->getItemByInput($args[1]);
-			if(!$object)
+			}
+			if(!$object) {
 				$object = $user->getItemByInput($args[1]);
-			if(!$object)
+			}
+			if(!$object) {
 				return Server::out($user, "That doesn't seem to exist.");
+			}
 		
 			$label = '';
-			if($object instanceof User)
+			if($object instanceof User) {
 				$label = $object->getAlias();
-			else if($object instanceof mItem)
+			}
+			else if($object instanceof mItem) {
 				$label = $object->getShort();
-		
-			$atts = $object->getAttributes();
-		
-			if(strpos('hp', $args[2]) === 0)
-			{
-				$atts->setHp($args[3]);
-				Server::out($user, "You set ".$label."'s hp to ".$args[3].".");
 			}
-			else if(strpos('mana', $args[2]) === 0)
-			{
-				$atts->setMana($args[3]);
-				Server::out($user, "You set ".$label."'s mana to ".$args[3].".");
+
+			if($object->setAttribute($args[2], $args[3])) {
+				Server::out($user, "You set ".$label."'s ".$args[2]." to ".$args[3].".");
+				if(method_exists($object, 'save')) {
+					$object->save();
+				}
+			} else {
+				Server::out($user, "They don't have that attribute.");
 			}
-			else if(strpos('movement', $args[2]) === 0)
-			{
-				$atts->setMovement($args[3]);
-				Server::out($user, "You set ".$label."'s movement to ".$args[3].".");
-			}
-			else if(strpos('str', $args[2]) === 0)
-			{
-				$atts->setStr($args[3]);
-				Server::out($user, "You set ".$label."'s str to ".$args[3].".");
-			}
-			else if(strpos('int', $args[2]) === 0)
-			{
-				$atts->setInt($args[3]);
-				Server::out($user, "You set ".$label."'s int to ".$args[3].".");
-			}
-			else if(strpos('wis', $args[2]) === 0)
-			{
-				$atts->setWis($args[3]);
-				Server::out($user, "You set ".$label."'s wis to ".$args[3].".");
-			}
-			else if(strpos('dex', $args[2]) === 0)
-			{
-				$atts->setDex($args[3]);
-				Server::out($user, "You set ".$label."'s dex to ".$args[3].".");
-			}
-			else if(strpos('con', $args[2]) === 0)
-			{
-				$atts->setCon($args[3]);
-				Server::out($user, "You set ".$label."'s con to ".$args[3].".");
-			}
-			else if(strpos('cha', $args[2]) === 0)
-			{
-				$atts->setCha($args[3]);
-				Server::out($user, "You set ".$label."'s cha to ".$args[3].".");
-			}
-			else if(strpos('ac_bash', $args[2]) === 0)
-			{
-				$atts->setAcBash($args[3]);
-				Server::out($user, "You set ".$label."'s bash ac to ".$args[3].".");
-			}
-			else if(strpos('ac_slash', $args[2]) === 0)
-			{
-				$atts->setAcSlash($args[3]);
-				Server::out($user, "You set ".$label."'s slash ac to ".$args[3].".");
-			}
-			else if(strpos('ac_pierce', $args[2]) === 0)
-			{
-				$atts->setStr($args[3]);
-				Server::out($user, "You set ".$label."'s pierce ac to ".$args[3].".");
-			}
-			else if(strpos('ac_magic', $args[2]) === 0)
-			{
-				$atts->setStr($args[3]);
-				Server::out($user, "You set ".$label."'s magic ac to ".$args[3].".");
-			}
-			else if(strpos('hit', $args[2]) === 0)
-			{
-				$atts->setHit($args[3]);
-				Server::out($user, "You set ".$label."'s hit roll to ".$args[3].".");
-			}
-			else if(strpos('dam', $args[2]) === 0)
-			{
-				$atts->setDam($args[3]);
-				Server::out($user, "You set ".$label."'s dam roll to ".$args[3].".");
-			}
-			else if(strpos('saves', $args[2]) === 0)
-			{
-				$atts->setSaves($args[3]);
-				Server::out($user, "You set ".$label."'s saves to ".$args[3].".");
-			}
-			if($object instanceof User && method_exists($object, 'save'))
-				$object->save();
 		}
 	}
 ?>
