f99c47130985059a7d9a397fece6cfb593c18e7a
Dan Munro <dan@danmunro.com>
2012-01-29 10:59:13 -0800

    updating game.php formatting/licensing

--- game.php
+++ game.php
@@ -1,32 +1,5 @@
 #!/usr/bin/php5
-
 <?php
-
-	/**
-	 *
-	 * Phud - a PHP implementation of the popular multi-user dungeon game paradigm.
-     * Copyright (C) 2009 Dan Munro
-	 * 
-     * This program is free software; you can redistribute it and/or modify
-     * it under the terms of the GNU General Public License as published by
-     * the Free Software Foundation; either version 2 of the License, or
-     * (at your option) any later version.
-	 * 
-     * This program is distributed in the hope that it will be useful,
-     * but WITHOUT ANY WARRANTY; without even the implied warranty of
-     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     * GNU General Public License for more details.
-	 * 
-     * You should have received a copy of the GNU General Public License along
-     * with this program; if not, write to the Free Software Foundation, Inc.,
-     * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-	 *
-	 * Contact Dan Munro at dan@danmunro.com
-	 * @author Dan Munro
-	 * @package Phud
-	 *
-	 */
-	
 	///////////////////////////////////////////////////////
 	// ADMIN CONFIG
 	///////////////////////////////////////////////////////
@@ -35,26 +8,23 @@
 	$port = 9000;
 
 	///////////////////////////////////////////////////////
-	// END admin config
+	// END admin config - nothing below here needs changing
+	// in order to run
 	///////////////////////////////////////////////////////
 	
-	// basic config
 	date_default_timezone_set('America/Los_Angeles');
 	gc_enable();
 	set_time_limit(0);
 	use \Mechanics\Debug;
 	use \Mechanics\Server;
-
-	// reset the logs
 	Debug::clearLog();
 
 	// initiate and run the server
 	$s = new Server($address, $port);
 	$s->run();
 	
-	// Autoloader
-	function __autoload($class)
-	{
+	// autoloader
+	function __autoload($class) {
 		$path = 'lib/'.str_replace("\\", "/", $class).".php";
 		if(file_exists($path))
 			require_once($path);
