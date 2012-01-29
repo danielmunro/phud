caf388b94347c450ca53ea0ce6c4fba7cd77a0d2
Dan Munro <dan@danmunro.com>
2012-01-29 11:40:45 -0800

    updating formatting on game.php

--- game.php
+++ game.php
@@ -1,32 +1,32 @@
 #!/usr/bin/php5
 <?php
-	///////////////////////////////////////////////////////
-	// ADMIN CONFIG
-	///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+// ADMIN CONFIG
+///////////////////////////////////////////////////////
 
-	$address = '192.168.0.106';
-	$port = 9000;
+$address = '192.168.0.106';
+$port = 9000;
 
-	///////////////////////////////////////////////////////
-	// END admin config - nothing below here needs changing
-	// in order to run
-	///////////////////////////////////////////////////////
-	
-	date_default_timezone_set('America/Los_Angeles');
-	gc_enable();
-	set_time_limit(0);
-	use \Mechanics\Debug;
-	use \Mechanics\Server;
-	Debug::clearLog();
+///////////////////////////////////////////////////////
+// END admin config - nothing below here needs changing
+// in order to run
+///////////////////////////////////////////////////////
 
-	// initiate and run the server
-	$s = new Server($address, $port);
-	$s->run();
-	
-	// autoloader
-	function __autoload($class) {
-		$path = 'lib/'.str_replace("\\", "/", $class).".php";
-		if(file_exists($path))
-			require_once($path);
-	}
+date_default_timezone_set('America/Los_Angeles');
+gc_enable();
+set_time_limit(0);
+use \Mechanics\Debug;
+use \Mechanics\Server;
+Debug::clearLog();
+
+// initiate and run the server
+$s = new Server($address, $port);
+$s->run();
+
+// autoloader
+function __autoload($class) {
+	$path = 'lib/'.str_replace("\\", "/", $class).".php";
+	if(file_exists($path))
+		require_once($path);
+}
 ?>
