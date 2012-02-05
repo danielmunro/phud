8bae539d5ed2d566d5ac6b4b9c41c4f1cc605bdb
Dan Munro <dan@danmunro.com>
2012-02-05 11:18:48 -0800

    Revising shield spell to use easyinit

--- deploy/init/Spells/Shield.php
+++ deploy/init/Spells/Shield.php
@@ -13,24 +13,25 @@ class Shield extends Spell
 	protected $required_proficiency = 25;
 	protected $saving_attribute = 'wis';
 
-	protected function success(Actor $actor)
+	protected function success(Actor $actor, Actor $target)
 	{
 		$timeout = min(30, ceil($proficiency / 2));
 		$mod_ac = min(-(round($proficiency / 1.5)), -30);
 		
-		$a = new Affect();
-		$a->setAffect(self::$name_familiar);
-		$a->setMessageAffect('Spell: shield: '.$mod_ac.' to armor class');
-		$a->setMessageEnd('You feel less protected.');
-		$a->setTimeout($timeout);
-		$atts = $a->getAttributes();
-		$atts->setAcBash($mod_ac);
-		$atts->setAcSlash($mod_ac);
-		$atts->setAcPierce($mod_ac);
-		$atts->setAcMagic($mod_ac);
-		$a->apply($target);
+		$a = new Affect([
+			'affect' => 'shield',
+			'message_affect' => 'Spell: shield: '.$mod_ac.' to armor class',
+			'message_end' => 'You feel less protected.',
+			'timeout' => $timeout,
+			'apply' => $target,
+			'attributes' => [
+				'ac_bash' => $mod_ac,
+				'ac_slash' => $mod_ac,
+				'ac_pierce' => $mod_ac,
+				'ac_magic' => $mod_ac
+			]
+		]);
 		Server::out($target, "You feel more protected!");
-		return false;
 	}
 }
 ?>
