e91f593a9683c08a3dd1a274ed67e8d6c6d84ee3
Dan Munro <dan@danmunro.com>
2012-02-10 06:47:32 -0800

    improving area script capabilities, including reading items now

--- lib/Mechanics/Area.php
+++ lib/Mechanics/Area.php
@@ -3,20 +3,24 @@ namespace Mechanics;
 class Area
 {
 	protected $fp = null;
+	protected $last_room = null;
 
 	public function __construct($area)
 	{
 		$this->fp = fopen($area, 'r');
 		while($line = $this->readLine()) {
 			switch($line) {
-				case 'rooms':
-					$this->loadRooms();
-					break;
+				case strpos($line, 'room') === 0:
+					$this->loadRoom(substr($line, strpos($line, ' ')+1));
+					break 1;
+				case strpos($line, 'item') === 0:
+					$this->loadItems();
+					break 1;
 			}
 		}
 	}
 
-	protected function loadRooms()
+	protected function loadRoom($id)
 	{
 		$getdir = function($d) {
 			$directions = ['north', 'south', 'east', 'west', 'up', 'down'];
@@ -26,38 +30,71 @@ class Area
 				}
 			}
 		};
-		while($line = $this->readLine()) {
-			$p = [];
-			$p['id'] = $line;
-			$p['title'] = $this->readLine();
-			$p['description'] = $this->readBlock();
-			$p['area'] = $this->readLine();
-			$line = $this->readLine();
-			while($line != "~" && $line) {
-				list($dir, $id) = explode(' ', $line);
-				$p[$getdir($dir)] = $id;
-				$line = $this->readLine();
+		$p = [];
+		$p['id'] = $id;
+		$p['title'] = $this->readLine();
+		$p['description'] = $this->readBlock();
+		$p['area'] = $this->readLine();
+		$line = $this->readLine();
+		$break = false;
+		while($line) {
+			if(substr($line, -1) === '~') {
+				$line = substr($line, 0, -1);
+				$break = true;
 			}
-			new Room($p);
+			list($dir, $id) = explode(' ', $line);
+			$p[$getdir($dir)] = $id;
+			if($break) {
+				break;
+			}
+			$line = $this->readLine();
 		}
+		$this->last_room = new Room($p);
+	}
+
+	protected function loadItems()
+	{
+		$p = [];
+		$class = $this->readLine();
+		$p['nouns'] = $this->readLine();
+		$p['short'] = $this->readLine();
+		$p['long'] = $this->readBlock();
+		$p['contents'] = $this->readLine();
+		$uses = $this->readLine();
+		if(substr($uses, -1) === '~') {
+			$p['uses'] = substr($uses, 0, -1);
+		} else {
+			$p['uses'] = $uses;
+			$this->loadItems();
+		}
+		$full_class = 'Items\\'.$class;
+		$this->last_room->addItem(new $full_class($p));
 	}
 
 	private function readLine()
 	{
-		$line = fgets($this->fp);
-		if(strpos($line, '#') === 0) {
+		$input = fgets($this->fp);
+		$line = trim($input);
+		if(strpos($line, '#') === 0 || (strlen($line) === 0 && $input !== false)) {
 			return $this->readLine();
 		}
-		return trim($line);
+		return $line;
 	}
 
 	private function readBlock()
 	{
 		$line = '';
 		$block = '';
-		while($line != "~") {
-			$line = $this->readLine();
+		$break = false;
+		while($line = $this->readLine()) {
+			if(substr($line, -1) === '~') {
+				$line = substr($line, 0, -1);
+				$break = true;
+			}
 			$block .= $line;
+			if($break) {
+				break;
+			}
 		}
 		return $block;
 	}
