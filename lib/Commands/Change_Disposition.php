8cdd1f85ec5dad9ee5aa7793ede83486e01282fa
Dan Munro <dan@danmunro.com>
2012-03-04 10:42:42 -0800

    Providing an abstract command class for changing an actor's disposition. This will provide consistent experiences when sitting/sleeping

---
+++ lib/Commands/Change_Disposition.php
@@ -0,0 +1,52 @@
+<?php
+namespace Commands;
+use \Mechanics\Command\Command,
+	\Mechanics\Actor,
+	\Mechanics\Server,
+	\Items\Furniture;
+
+abstract class Change_Disposition extends Command
+{
+	protected $dispositions = [
+		Actor::DISPOSITION_STANDING,
+		Actor::DISPOSITION_SITTING,
+		Actor::DISPOSITION_SLEEPING
+	];
+
+	protected function changeDisposition(Actor $actor, $args, $verb)
+	{
+		$disposition = $actor->getDisposition();
+		$actor->setDisposition($verb);
+		if(sizeof($args) > 1) {
+			$furniture = $actor->getRoom()->getItemByInput($args[1]);
+			if($furniture instanceof Furniture) {
+				if($furniture->hasCapacity($actor)) {
+					$furniture->addActor($actor);
+					return Server::out($actor, "You ".$this->leaveDisposition($disposition)."begin ".$verb." on ".$furniture.".");
+				} else {
+					return Server::out($actor, ucfirst($furniture)." is full right now.");
+				}
+			} else if($furniture) {
+				return Server::out($actor, "You can't ".$this->alias." on that!");
+			} else {
+				return Server::out($actor, "You can't find it.");
+			}
+		} else {
+			Server::out($actor, "You ".$this->leaveDisposition($disposition)."begin ".$verb.".");
+		}
+	}
+	
+	protected function leaveDisposition($disposition)
+	{
+		switch($disposition) {
+			case 'sitting':
+				return "stand up and ";
+			case 'sleeping':
+				return "wake up, stand up and ";
+			default:
+				return;
+		}
+	}
+}
+
+?>
--- lib/Commands/Sleep.php
+++ lib/Commands/Sleep.php
@@ -1,23 +1,14 @@
 <?php
 namespace Commands;
-use \Mechanics\Alias,
-	\Mechanics\Server,
-	\Mechanics\Actor,
-	\Mechanics\Command\Command;
+use \Mechanics\Actor;
 	
-class Sleep extends Command
+class Sleep extends Change_Disposition
 {
 	protected $alias = 'sleep';
-	protected $dispositions = [Actor::DISPOSITION_SLEEPING];
 
-	public function perform(Actor $actor, $args = array())
+	public function perform(Actor $actor, $args = [])
 	{
-		if($actor->getDisposition() === Actor::DISPOSITION_SLEEPING)
-			return Server::out($actor, "You are already sleeping.");
-		
-		Server::out($actor, "You lie down and go to sleep.");
-		$actor->getRoom()->announce($actor, ucfirst($actor)." lies down and goes to sleep.");
-		$actor->setDisposition(Actor::DISPOSITION_SLEEPING);
+		$this->changeDisposition($actor, $args, 'sleeping');
 	}
 
 }
