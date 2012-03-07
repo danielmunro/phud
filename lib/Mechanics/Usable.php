89c7cc10587e4c0523ed8d141cb7f9d82bdf01af
Dan Munro <dan@danmunro.com>
2012-03-07 06:21:07 -0800

    Usable can now find all matches in addition to the first match

--- lib/Mechanics/Usable.php
+++ lib/Mechanics/Usable.php
@@ -5,17 +5,35 @@ trait Usable
 {
 	public function getUsableByInput($collection, $input)
 	{
+		return $this->getUsables($collection, $input, true);
+	}
+
+	public function getManyUsablesByInput($collection, $input)
+	{
+		return $this->getUsables($collection, $input, false);
+	}
+
+	protected function getUsables($collection, $input, $scalar)
+	{
+		$found = [];
 		foreach($collection as $c) {
-			foreach($this->getUsables($c) as $u) {
+			foreach($this->getUsableProperty($c) as $u) {
 				if(stripos($u, $input) === 0) {
-					return $c;
+					if($scalar) {
+						return $c;
+					} else {
+						$found[] = $c;
+					}
 				}
 			}
 		}
-		return false;
+		if($scalar && !$found) {
+			return false;
+		}
+		return $found;
 	}
 
-	protected function getUsables($usable)
+	protected function getUsableProperty($usable)
 	{
 		return property_exists($usable, 'nouns') ? explode(' ', $usable->getNouns()) : [$usable->getAlias()];
 	}
