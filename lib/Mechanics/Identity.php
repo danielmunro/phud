45259067796a0444db5d27e9fbcb3a1a941f085b
Dan Munro <dan@danmunro.com>
2012-03-07 06:19:42 -0800

    Adding new trait for things in the mud that need ids (actors, items, rooms, etc)

---
+++ lib/Mechanics/Identity.php
@@ -0,0 +1,21 @@
+<?php
+namespace Mechanics;
+
+trait Identity
+{
+	protected $id = '';
+	protected static $identities = [];
+
+	public static function getByID($id)
+	{
+		if(isset(static::$identities[$id]) && static::$identities[$id] instanceof static) {
+			return static::$identities[$id];
+		}
+	}
+
+	public function getID()
+	{
+		return $this->id;
+	}
+}
+?>
