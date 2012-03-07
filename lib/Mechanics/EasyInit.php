061b0c982208d847314faf657268d089ff4c3fc4
Dan Munro <dan@danmunro.com>
2012-03-07 06:44:28 -0800

    ignoring a log warning that is a result of type confusion, a specific common case

--- lib/Mechanics/EasyInit.php
+++ lib/Mechanics/EasyInit.php
@@ -12,7 +12,7 @@ trait EasyInit
 			} else if(property_exists($this, $property)) {
 				$t1 = gettype($value);
 				$t2 = gettype($this->$property);
-				if($t1 !== $t2) {
+				if($t1 !== $t2 && ($t1 !== 'integer' && $t2 !== 'string')) {
 					Debug::log('Property ('.$property.') of '.$this.' has a type mismatch, expecting ('.$t2.'), got ('.$t1.')');
 				}
 				$this->$property = $value;
