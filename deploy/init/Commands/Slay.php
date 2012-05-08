78bceff58646821d807a57cd05735ad1d1638a44
Dan Munro <dan@danmunro.com>
2012-05-08 06:14:08 -0700

    Fixing a bug in slay where it was referencing an old, removed function

--- deploy/init/Commands/Slay.php
+++ deploy/init/Commands/Slay.php
@@ -7,12 +7,16 @@ class Slay extends DM
 {
 	protected $alias = 'slay';
 
-	public function perform(lUser $user, $args = array())
+	public function perform(lUser $user, $args = [])
 	{
 		$target = $user->getRoom()->getActorByInput($args[1]);
 		Server::out($user, "You slay ".$target." in cold blood!");
-		$user->getRoom()->announce($user, ucfirst($user)." slays ".$target." in cold blood!");
-		$target->delete();
+		$user->getRoom()->announce([
+			['actor' => $target, 'message' => ucfirst($user)." slays you in cold blood!"],
+			['actor' => $user, 'message' => "You slay ".$target." in cold blood!"],
+			['actor' => '*', 'message' => ucfirst($user)." slays ".$target." in cold blood!"]
+		]);
+		$user->setAttribute('hp', 0);
 	}
 }
 ?>
