24629dd865cc020d449cce650fe2bc3f7dedfc5b
Dan Munro <dan@danmunro.com>
2012-05-14 19:02:10 -0700

    cleaning up score command for more changes later

--- deploy/init/Commands/Score.php
+++ deploy/init/Commands/Score.php
@@ -7,61 +7,51 @@ class Score extends User
 {
 	protected $alias = 'score';
 
-	public function perform(lUser $user, $args = array())
+	public function perform(lUser $user, $args = [])
 	{
-		Server::out($user, 'You are ' . $user . ', a ' . $user->getRace()['alias']);
-		Server::out($user, 'Attributes: '.
-			'Str '.$user->getAttribute('str').'('.$user->getUnmodifiedAttribute('str').') ' .
-			'Int '.$user->getAttribute('int').'('.$user->getUnmodifiedAttribute('int').') ' . 
-			'Wis '.$user->getAttribute('wis').'('.$user->getUnmodifiedAttribute('wis').') ' .
-			'Dex '.$user->getAttribute('dex').'('.$user->getUnmodifiedAttribute('dex').') ' .
-			'Con '.$user->getAttribute('con').'('.$user->getUnmodifiedAttribute('con').') ' .
-			'Cha '.$user->getAttribute('cha').'('.$user->getUnmodifiedAttribute('cha').')');
-		
-		Server::out(
-			$user, 'Hp: ' . $user->getAttribute('hp') . '/' . $user->getMaxAttribute('hp') .
-			' Mana: ' . $user->getAttribute('mana') . '/' . $user->getMaxAttribute('mana') .
-			' Movement: ' . $user->getAttribute('movement') . '/' . $user->getMaxAttribute('movement'));
-		
-		//$experience = round($user->getExperiencePerLevel() - ($user->getExperience() % $user->getExperiencePerLevel()));
-		$experience = 1;
-		Server::out($user,
-			'Level ' . $user->getLevel() . ', ' . $experience . ' experience to next level');
-		Server::out($user,
-			$user->getCurrency('gold') . ' gold, ' . $user->getCurrency('silver') . ' silver, ' . $user->getCurrency('copper') . ' copper.');
-	
-		Server::out($user, 'You are' . self::getAcString($user->getAttribute('ac_bash')) . 'against bashing.');
-		Server::out($user, 'You are' . self::getAcString($user->getAttribute('ac_slash')) . 'against slashing.');
-		Server::out($user, 'You are' . self::getAcString($user->getAttribute('ac_pierce')) . 'against piercing.');
-		Server::out($user, 'You are' . self::getAcString($user->getAttribute('ac_magic')) . 'against magic.');
-	
+		$out_atts = '';
+		foreach(['str', 'int', 'wis', 'dex', 'con', 'cha'] as $att) {
+			$out_atts .= ucfirst($att).' '.$user->getAttribute($att).' ('.$user->getUnmodifiedAttribute($att).') ';
+		}
+		Server::out($user, "You are ".$user.", a ".$user->getRace()['alias']."\n".
+			"Attributes:\n".
+			$out_atts."\n".
+			"Hp: ".$user->getAttribute('hp')."/".$user->getMaxAttribute('hp').
+			" Mana: ".$user->getAttribute('mana')."/".$user->getMaxAttribute('mana').
+			" Movement: ".$user->getAttribute('movement')."/".$user->getMaxAttribute('movement')."\n".
+			"Level ".$user->getLevel().", 1 experience to next level\n".
+			$user->getCurrency('gold')." gold, ".$user->getCurrency('silver')." silver, ".$user->getCurrency('copper')." copper.\n".
+			"You are ".self::getAcString($user->getAttribute('ac_bash'))." against bashing.\n".
+			"You are ".self::getAcString($user->getAttribute('ac_slash'))." against slashing.\n".
+			"You are ".self::getAcString($user->getAttribute('ac_pierce'))." against piercing.\n".
+			"You are ".self::getAcString($user->getAttribute('ac_magic'))." against magic.");
 	}
 	
 	private static function getAcString($ac)
 	{
 		if($ac >= 100)
-			return " hopelessly vulnerable to ";
+			return "hopelessly vulnerable to";
 		if($ac >= 80)
-			return " defenseless against ";
+			return "defenseless against";
 		if($ac >= 60)
-			return " barely protected from ";
+			return "barely protected from";
 		if($ac >= 40)
-			return " slightly armored against ";
+			return "slightly armored against";
 		if($ac >= 20)
-			return " somewhat armored against ";
+			return "somewhat armored against";
 		if($ac >= 0)
-			return " armored against ";
+			return "armored against";
 		if($ac >= -20)
-			return " well-armored against ";
+			return "well-armored against";
 		if($ac >= -40)
-			return " very well-armored against ";
+			return "very well-armored against";
 		if($ac >= -60)
-			return " heavily armored against ";
+			return "heavily armored against";
 		if($ac >= -80)
-			return " superbly armored against ";
+			return "superbly armored against";
 		if($ac >= -100)
-			return " almost invulnerable to ";
-		return " divinely armored against ";
+			return "almost invulnerable to";
+		return "divinely armored against";
 	}
 }
 ?>
