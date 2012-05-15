996d60ba89c24ffe67156b575e709a19d4b4b51a
Dan Munro <dan@danmunro.com>
2012-05-15 06:36:19 -0700

    Add trains and practices to score and make formatting a litte more consistent

--- deploy/init/Commands/Score.php
+++ deploy/init/Commands/Score.php
@@ -12,17 +12,18 @@ class Score extends User
 		Server::out($user, "You are ".$user.", a ".$user->getRace()['alias']."\n".
 			"Attributes:\n".
 			implode(' ', array_map(function($v) use ($user) {
-				return ucfirst($v)." ".$user->getAttribute($v)." (".$user->getUnmodifiedAttribute($v).") ";
+				return ucfirst($v)." ".$user->getAttribute($v)." (".$user->getUnmodifiedAttribute($v).")";
 			}, ['str', 'int', 'wis', 'dex', 'con', 'cha']))."\n".
 			"Hp: ".$user->getAttribute('hp')."/".$user->getMaxAttribute('hp').
 			" Mana: ".$user->getAttribute('mana')."/".$user->getMaxAttribute('mana').
 			" Movement: ".$user->getAttribute('movement')."/".$user->getMaxAttribute('movement')."\n".
+			"Trains: ".$user->getTrains().", practices: ".$user->getPractices()."\n".
 			"Level ".$user->getLevel().", 1 experience to next level\n".
-			$user->getCurrency('gold')." gold, ".$user->getCurrency('silver')." silver, ".$user->getCurrency('copper')." copper.\n".
-			"You are ".self::getAcString($user->getAttribute('ac_bash'))." against bashing.\n".
-			"You are ".self::getAcString($user->getAttribute('ac_slash'))." against slashing.\n".
-			"You are ".self::getAcString($user->getAttribute('ac_pierce'))." against piercing.\n".
-			"You are ".self::getAcString($user->getAttribute('ac_magic'))." against magic.");
+			$user->getCurrency('gold')." gold, ".$user->getCurrency('silver')." silver, ".$user->getCurrency('copper')." copper\n".
+			"You are ".self::getAcString($user->getAttribute('ac_bash'))." against bashing\n".
+			"You are ".self::getAcString($user->getAttribute('ac_slash'))." against slashing\n".
+			"You are ".self::getAcString($user->getAttribute('ac_pierce'))." against piercing\n".
+			"You are ".self::getAcString($user->getAttribute('ac_magic'))." against magic");
 	}
 	
 	private static function getAcString($ac)
