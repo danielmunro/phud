fce8668289a70496495f06f7a2b01f8307b6202d
Dan Munro <dan@danmunro.com>
2012-05-28 12:37:01 -0700

    Removing more verbose debugging, only track the namespace instantiation, not each class

--- lib/Instantiate.php
+++ lib/Instantiate.php
@@ -8,9 +8,9 @@ trait Instantiate
 		global $global_path;
 		list($phud, $namespace, $class) = explode('\\', get_called_class());
 		$d = dir($global_path.'/deploy/init/'.$namespace);
+		Debug::log("init ".$namespace);
 		while($class = $d->read()) {
 			if(substr($class, -4) === ".php") {
-				Debug::log("init ".$namespace.": ".$class);
 				$class = substr($class, 0, strpos($class, '.'));
 				$called_class = 'Phud\\'.$namespace.'\\'.$class;
 				new $called_class();
