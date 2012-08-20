9820f7651ce79fe4b7abee2eaa6fb59a5c656a69
Dan Munro <dan@danmunro.com>
2012-08-20 06:45:34 -0700

    Making args nullable in Arguments\Shopkeeper Default null behavior is to look for a shopkeeper in the same room as the actor.

--- lib/Commands/Arguments/Shopkeeper.php
+++ lib/Commands/Arguments/Shopkeeper.php
@@ -5,13 +5,20 @@ use Phud\Actors\Actor as aActor,
 
 class Shopkeeper extends Argument
 {
-	protected function parseArg(aActor $actor, $arg)
+	protected function parseArg(aActor $actor, $arg = null)
 	{
-		$target = $actor->getRoom()->getActorByInput($arg);
-		if($target instanceof aShopkeeper) {
-			return $target;
+		if($arg === null) {
+			foreach($actor->getRoom()->getActors() as $_actor) {
+				if($_actor instanceof aShopkeeper) {
+					return $_actor;
+				}
+			}
+		} else {
+			$target = $actor->getRoom()->getActorByInput($arg);
+			if($target instanceof aShopkeeper) {
+				return $target;
+			}
 		}
-		$this->status = self::STATUS_INVALID;
-		$actor->notify("No one is there.");
+		$this->fail($actor, "No one is there.");
 	}
 }
