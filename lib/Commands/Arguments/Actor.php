e1e82dbdcd87d2c18e1642005316703782f706ad
Dan Munro <dan@danmunro.com>
2012-08-22 19:44:33 -0700

    Tell the actor argument where to look for the actor

--- lib/Commands/Arguments/Actor.php
+++ lib/Commands/Arguments/Actor.php
@@ -1,11 +1,17 @@
 <?php
 namespace Phud\Commands\Arguments;
+use Phud\Room\Room;
 
 class Actor extends Argument
 {
+	public function __construct(Room $search_in)
+	{
+		$this->search_in = $search_in;
+	}
+
 	public function parse($arg)
 	{
-		$target = $actor->getRoom()->getActorByInput($arg);
+		$target = $this->search_in->getActorByInput($arg);
 		if($target) {
 			return $target;
 		}
